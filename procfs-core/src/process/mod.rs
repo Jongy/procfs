9fa6965baba711e39c24313cdfb13854c70e042f
Yonatan Goldschmidt <yonatan.goldschmidt@wiz.io>
2025-03-01 01:14:38 +0200

    Derive PartialEq for FDTarget

--- procfs-core/src/process/mod.rs
+++ procfs-core/src/process/mod.rs
@@ -687,7 +687,7 @@ impl crate::FromBufRead for Io {
 }
 
 /// Describes a file descriptor opened by a process.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
 pub enum FDTarget {
     /// A file or device
