e565e11ae71fe3486c29970e81f60d2bdfb31774
Yonatan Goldschmidt <yonatan.goldschmidt@wiz.io>
2025-03-09 01:24:12 +0200

    Add Process.open_relative_flags

--- procfs/src/process/mod.rs
+++ procfs/src/process/mod.rs
@@ -870,6 +870,15 @@ impl Process {
         Ok(file.inner())
     }
 
+    /// Returns a file which is part of the process proc structure
+    pub fn open_relative_flags<P>(&self, path: P, flags: OFlags) -> ProcResult<File>
+    where
+        P: AsRef<Path>,
+    {
+        let file = FileWrapper::open_at_flags(&self.root, &self.fd, path, flags)?;
+        Ok(file.inner())
+    }
+
     /// Parse a file relative to the process proc structure.
     pub fn read<T: FromRead, P>(&self, path: P) -> ProcResult<T>
     where
